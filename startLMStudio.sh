99544d56938b46957ba5c8d5e2032d92b67dd1c2
techupskills <82771267+techupskills@users.noreply.github.com>
2024-06-04 13:57:56 -0400

    Create startLMStudio.sh

---
+++ startLMStudio.sh
@@ -0,0 +1,4 @@
+wget https://releases.lmstudio.ai/linux/0.2.15/beta/LM_Studio-0.2.15-beta-1.AppImage
+mv LM_Studio-0.2.15-beta-1.AppImage LM_Studio.AppImage
+chmod +x LM_Studio.AppImage
+./LM_Studio.AppImage &
