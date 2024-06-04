e2b68dc1e83241da94ced5e35a1d1f1f515c75e2
techupskills <82771267+techupskills@users.noreply.github.com>
2024-06-04 15:26:23 -0400

    Update startLMStudio.sh

--- startLMStudio.sh
+++ startLMStudio.sh
@@ -1,4 +1,4 @@
-wget https://releases.lmstudio.ai/linux/0.2.22/beta/LM_Studio-0.2.22-beta-1.AppImage
+wget https://releases.lmstudio.ai/linux/0.2.22/beta/LM_Studio-0.2.22.AppImage
 mv LM_Studio-0.2.22-beta-1.AppImage LM_Studio.AppImage
 chmod +x LM_Studio.AppImage
 ./LM_Studio.AppImage &
