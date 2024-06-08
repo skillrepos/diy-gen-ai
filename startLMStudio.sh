8a6b4d044f8dd072f19976ea68619f2fc3ca75ec
techupskills <82771267+techupskills@users.noreply.github.com>
2024-06-08 19:40:21 -0400

    Update startLMStudio.sh

--- startLMStudio.sh
+++ startLMStudio.sh
@@ -2,6 +2,5 @@ if ! [ -e LM_Studio.AppImage ]; then
     wget https://releases.lmstudio.ai/linux/0.2.22/beta/LM_Studio-0.2.22.AppImage
     mv LM_Studio-0.2.22.AppImage LM_Studio.AppImage
     chmod +x LM_Studio.AppImage
-else
-    ./LM_Studio.AppImage &
 fi
+./LM_Studio.AppImage &
