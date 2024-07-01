54346373d8724a99ecb6492a06af3cca5fb7efd5
techupskills <82771267+techupskills@users.noreply.github.com>
2024-07-01 16:07:11 -0400

    Update startLMStudio.sh

--- startLMStudio.sh
+++ startLMStudio.sh
@@ -4,3 +4,5 @@ if ! [ -e LM_Studio.AppImage ]; then
     chmod +x LM_Studio.AppImage
 fi
 ./LM_Studio.AppImage &
+printf "Your application can now be accessed at \e]8;;https://"$CODESPACE_NAME"-5800.app.github.dev/vnc.html?path=vnc&autoconnect=true&resize=scale&reconnect=true&show_dot=true&bell=false&view_only=false\e\\LM Studio\e]8;;\e"
+printf '\\\n'
