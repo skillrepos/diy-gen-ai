3c8c2a452a4c6129e1bc7bdd05b3421f90a9bb86
techupskills <82771267+techupskills@users.noreply.github.com>
2024-07-01 16:23:54 -0400

    Update startLMStudio.sh

--- startLMStudio.sh
+++ startLMStudio.sh
@@ -1,3 +1,4 @@
+docker compose up -d
 if ! [ -e LM_Studio.AppImage ]; then
     wget https://releases.lmstudio.ai/linux/0.2.22/beta/LM_Studio-0.2.22.AppImage
     mv LM_Studio-0.2.22.AppImage LM_Studio.AppImage
