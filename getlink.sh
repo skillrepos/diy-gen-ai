5ee6defbf41f445aec6b3dd8292f0f4abed7a8ca
techupskills <82771267+techupskills@users.noreply.github.com>
2024-07-01 14:37:55 -0400

    Create getlink.sh

---
+++ getlink.sh
@@ -0,0 +1,2 @@
+printf "Your application can now be accessed at \e]8;;https://"$CODESPACE_NAME"-5800.app.github.dev/vnc.html?path=vnc&autoconnect=true&resize=scale&reconnect=true&show_dot=true&bell=false&view_only=false\e\\LM Studio\e]8;;\e"
+printf '\\\n'
