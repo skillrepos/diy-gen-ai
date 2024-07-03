da44dece30e3e9131cd8207ce99ef9769306adee
Brent Laster <bclaster@nclasters.org>
2024-07-03 22:24:31 +0000

    updates for latest version

---
+++ scripts/startLMStudio.sh
@@ -0,0 +1,7 @@
+docker compose up -d
+if ! [ -e LM_Studio.AppImage ]; then
+    wget https://releases.lmstudio.ai/linux/x86/0.2.26/beta/LM_Studio-0.2.26.AppImage
+    mv LM_Studio-0.2.26.AppImage LM_Studio.AppImage
+    chmod +x LM_Studio.AppImage
+fi
+./LM_Studio.AppImage &
