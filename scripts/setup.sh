64cd0a7a616eacb9b9751bb759f633220072cb1c
techupskills <82771267+techupskills@users.noreply.github.com>
2024-09-30 20:07:48 -0400

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,4 @@
+set -x
 sudo apt update && sudo apt upgrade -y
 sudo apt install meson ninja-build ffmpeg fuse pciutils -y
 wget https://github.com/libfuse/libfuse/releases/download/fuse-3.16.2/fuse-3.16.2.tar.gz
@@ -10,5 +11,5 @@ sudo ninja install
 sudo apt install libgconf-2-4 libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 libgbm-dev libnss3-dev libxss-dev libasound2 -y
 cd /workspaces/diy-gen-ai
 rm -rf fuse-3.16.2
-rm fuse-3.16.2.tar.gz
+
 
