50a5ada2ffc29bcb773853c59d81b7568bee7c95
techupskills <82771267+techupskills@users.noreply.github.com>
2024-12-27 11:15:03 -0500

    Update startLMStudio.sh

--- scripts/startLMStudio.sh
+++ scripts/startLMStudio.sh
@@ -1,7 +1,7 @@
 set -x
 docker compose up -d
 if ! [ -e LM_Studio.AppImage ]; then
-    wget https://releases.lmstudio.ai/linux/x86/0.3.5/beta/LM_Studio-0.3.5.AppImage
+    wget https://releases.lmstudio.ai/linux/x86/0.3.5/2/LM_Studio-0.3.5.AppImage
     mv LM_Studio-0.3.5.AppImage LM_Studio.AppImage
     chmod +x LM_Studio.AppImage
 fi
